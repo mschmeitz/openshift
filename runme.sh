1d1e24a978db08fba8f51abd48535b0e4af8b3ce
mschmeitz <mike.schmeitz@ictelligent.nl>
2019-05-09 21:40:50 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -2,6 +2,6 @@
 echo "This is a sample script"
 echo "-----------------------"
 echo "Parameter equals $PARAMETER"
-echo "The total is:"
+echo "The total is approx.:"
 echo "$NUM1+$NUM2"
 echo "------------------------"
